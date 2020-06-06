67307c4738fae0caf162fbf635703c6ebf59b44d
Muhammad Junaid <m.junaidcs@gmail.com>
2020-06-06 22:25:26 +0500

    author input

--- src/authors/inputs/create-author.input.ts
+++ src/authors/inputs/create-author.input.ts
@@ -1,5 +1,13 @@
+import { IsString, IsInt } from 'class-validator';
+
 export class CreateAuthorInput {
+
+  @IsInt()
   age: number;
+  
+  @IsString()
   name: string;
+  
+  @IsInt()
   rating: number;
 }
