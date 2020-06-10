9fd2e90f31171368edd32f829d9dd958d3efbfd1
Muhammad Junaid <m.junaidcs@gmail.com>
2020-06-10 20:48:35 +0500

    constants

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthorsModule],
+  imports: [AuthorsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
