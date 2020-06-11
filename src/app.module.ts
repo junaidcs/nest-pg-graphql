579f74c3d1b30a8784bbe38f185e3493718d947d
Muhammad Junaid <m.junaidcs@gmail.com>
2020-06-11 22:05:51 +0500

    typings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthorsModule, AuthModule, UsersModule],
+  imports: [
+    AuthorsModule,
+    // AuthModule,
+    UsersModule,
+    GraphQLModule.forRoot({
+      debug: true,
+      tracing: true,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
